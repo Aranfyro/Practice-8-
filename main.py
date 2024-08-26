bbdc76adb710e2e9f433caee7e92115dc3352d42
Aranfyro <141035777+Aranfyro@users.noreply.github.com>
2024-08-26 21:41:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-list_students = list(students)
+list_students = sorted(students)
 
 average = [sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]),
            sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4])]
